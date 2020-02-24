a2e31fa14f4ff0d4e891009620e3b64e7b9cb1ba
emily <emily@emilyshow.com>
2020-02-24 12:18:17 -0800

    redesign

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,17 @@
-// Custom Script
-// Developed by: Samson.Onna
+
+//header
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
 var customScripts = {
     profile: function () {
         // portfolio
